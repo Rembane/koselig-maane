d3620739a1ff72014f3792e84d37e972c0a95c90
Andreas Ekeroot <andeke@gmail.com>
2024-01-27 18:19:40 +0100

    Add wolf spawned at a random position
    Mostly to see that it works. This can be refined so much.

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,9 +5,16 @@ extends Node
 
 var clicking = false
 var player
+var wolf
 
 func _ready():
 	player = find_child("Player")
+	wolf = find_child("Wolf")
+
+	var rect = get_viewport().get_visible_rect()
+	var rect_p = rect.position
+	var rect_e = rect.end
+	wolf.position = Vector2(randf_range(rect_p.x, rect_e.x), randf_range(rect_p.y, rect_e.y))
 
 func _unhandled_input(event):
 	if Input.is_action_pressed("mouse_left_click") and clicking == false:
