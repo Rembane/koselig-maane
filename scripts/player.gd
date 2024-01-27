24c7f4ee33b5151fe2f32534b8e511527c5bf6ba
Andreas Ekeroot <andeke@gmail.com>
2024-01-27 18:24:27 +0100

    Set player sprite, and player walking sprite
    Move the player to stand on the ground, and not be embedded in it.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 func _ready():
 	var screen_size = get_viewport_rect().size
-	position = Vector2(0, screen_size.y)
+	var collision_size = find_child("CollisionShape2D").get_shape().get_rect().size
+	position = Vector2(collision_size.x / 2, screen_size.y - collision_size.y / 2)
 
 func _process(delta):
 	pass
